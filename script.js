d2db727c652cc03229c47833751a27a37ccf4e8c
Isabellagg19 <isabellaguioc123@gmail.com>
2026-05-29 16:15:20 -0500

    new sounds in the pad

--- script.js
+++ script.js
@@ -4,31 +4,24 @@ audio.load();
 audio.play();
 }
 
-function ripple1(pad, e) {
-    const rect = pad.getBoundingClientRect();
-    const size = Math.max(rect.width, rect.height);
-    const x = e.clientX - rect.left - size /2;
-    const y = e.clientY - rect.top - size /2;
+function triggerRipple(pad, e) {
+  const rect = pad.getBoundingClientRect();
+  const size = Math.max(rect.width, rect.height);
+  const x = e.clientX - rect.left - size / 2;
+  const y = e.clientY - rect.top  - size / 2;
 
-const ripple = document.createElement('span');
-ripple.classList.add('ripple-el');
-ripple.style.cssText = `
-width: ${size}px;
-height: ${size}px;
-left: ${x}px;
-top: ${y}px;
-`;
-
-pad.appendChild(ripple);
-ripple.addEventListener('animationed', () => ripple.remove());
+  const ripple = document.createElement('span');
+  ripple.classList.add('ripple-el');
+  ripple.style.cssText = `
+    width: ${size}px;
+    height: ${size}px;
+    left: ${x}px;
+    top: ${y}px;
+  `;
+  pad.appendChild(ripple);
+  ripple.addEventListener('animationend', () => ripple.remove());
 }
 
 document.querySelectorAll('.box').forEach(pad => {
-    pad.addEventListener('click', e => triggerRipple(pad,e));
+  pad.addEventListener('click', e => triggerRipple(pad, e));
 });
-
-
-
-
-
-
