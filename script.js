d389fafa11852688cb2cd4c34b71409c8a829035
Isabellagg19 <isabellaguioc123@gmail.com>
2026-05-29 07:40:49 -0500

    new rimpple function

--- script.js
+++ script.js
@@ -4,3 +4,31 @@ audio.load();
 audio.play();
 }
 
+function ripple1(pad, e) {
+    const rect = pad.getBoundingClientRect();
+    const size = Math.max(rect.width, rect.height);
+    const x = e.clientX - rect.left - size /2;
+    const y = e.clientY - rect.top - size /2;
+
+const ripple = document.createElement('span');
+ripple.classList.add('ripple-el');
+ripple.style.cssText = `
+width: ${size}px;
+height: ${size}px;
+left: ${x}px;
+top: ${y}px;
+`;
+
+pad.appendChild(ripple);
+ripple.addEventListener('animationed', () => ripple.remove());
+}
+
+document.querySelectorAll('.box').forEach(pad => {
+    pad.addEventListener('click', e => triggerRipple(pad,e));
+});
+
+
+
+
+
+
